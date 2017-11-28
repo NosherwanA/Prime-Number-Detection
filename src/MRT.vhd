0cc75c7f5c112665d2bb603aa802a2552fa98361
Nosherwan Ahmed <nosherwan.a2@gmail.com>
2017-11-28 13:38:59 -0500

    Created the input and output ports for the entity

--- src/MRT.vhd
+++ src/MRT.vhd
@@ -4,7 +4,13 @@ use ieee.numeric_std.all;
 
 entity MRT is
     port(
-
+        numberToCheck               : in std_logic_vector (7 downto 0);
+        clk                         : in std_logic;
+        reset                       : in std_logic;
+        start                       : in std_logic;
+        isPrime                     : out std_logic;
+        busy                        : out std_logic;
+        done                        : out std_logic
     );
 end entity;
 
