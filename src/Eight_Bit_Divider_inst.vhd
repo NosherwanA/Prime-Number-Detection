393dda096aa71da56cc22247c96405bc63f7ab22
NosherwanA <nosherwan.a2@gmail.com>
2017-12-03 13:33:31 -0500

    Removed Eight_Bit_Divider

--- src/Eight_Bit_Divider_inst.vhd
+++
@@ -1,9 +0,0 @@
-Eight_Bit_Divider_inst : Eight_Bit_Divider PORT MAP (
-		aclr	 => aclr_sig,
-		clken	 => clken_sig,
-		clock	 => clock_sig,
-		denom	 => denom_sig,
-		numer	 => numer_sig,
-		quotient	 => quotient_sig,
-		remain	 => remain_sig
-	);
